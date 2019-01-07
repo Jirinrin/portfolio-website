5c520773cfadda67816f22df31f9f99ac2d0a901
Jirinrin <jiri.ei@hotmail.com>
2019-01-07 21:47:40 +0100

    Got basic tooltips working

--- src/components/Landscape1.jsx
+++ src/components/Landscape1.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
 
 const BOOK_HEIGHT = 35;
 const BOOK_WIDTH = 215;
@@ -7,14 +8,14 @@ const BOOK_BASE_LEFT = 3690;
 const BOOK_BASE_BOTTOM = 4300;
 
 const objectsReference = [
-  ['awards-cup',        2811, 3594],
-  ['future-building',   479.5, 3765],
-  ['hobby-heap',        74,   4377],
-  ['jiri-soul',         2103, 4330],
-  ['octopus-tree',      1321, 4205],
-  ['spiral-tower',      4238, 2760],
-  ['technology-forest', 0,    3919],
-  ['book-stack',        BOOK_BASE_LEFT, BOOK_BASE_BOTTOM]
+  ['awards-cup',        2811,  3594, 'Awards'],
+  ['future-building',   479.5, 3765, 'Future Fantasy'],
+  ['hobby-heap',        74,    4377, 'Hobby Heap'],
+  ['jiri-soul',         2103,  4330, 'Jiri\'s SOUL'],
+  ['octopus-tree',      1321,  4205, 'The Octopus Tree of Life'],
+  ['spiral-tower',      4238,  2760, 'Two Sided Miracle'],
+  ['technology-forest', 0,     3919, 'Techno Forest'],
+  ['book-stack',        null,  null, 'Projects']
 ];
 
 const getBookStackTop = (size) => BOOK_BASE_BOTTOM - size * BOOK_HEIGHT;
@@ -23,7 +24,8 @@ class Landscape extends Component {
   state = {
     objects: [],
     bookStack: [],
-    toolTip: null,
+    tooltip: null,
+    showTooltip: false,
     popupMessage: null
   };
 
@@ -40,7 +42,8 @@ class Landscape extends Component {
       objects: objectsReference.map(obj => {
         const bs = obj[0] === 'book-stack';
         return {
-          name: obj[0], 
+          id: obj[0],
+          name: obj[3],
           left: bs ? BOOK_BASE_LEFT                             : obj[1],
           top: bs ? getBookStackTop(this.props.projects.length) : obj[2],
           width: bs ? BOOK_WIDTH                                : null,
@@ -83,13 +86,23 @@ class Landscape extends Component {
     }
   }
 
-  showToolTip = () => {
-    return;
-  }
+  showTooltip = (e) => {
+    const target = e.target.id ? e.target : e.target.parentNode;
 
-  hideToolTip = () => {
-    return;
+    this.setState({
+      showTooltip: true,
+      tooltip: {
+        contents: target.name,
+        left: target.style.left,
+        top: parseInt(target.style.top) - target.naturalHeight * 0.6,
+        fontSize: + 1 / this.props.scaleFactor + 'rem'
+      }
+    });
   }
+  
+  
+
+  hideTooltip = () => this.setState({showTooltip: false});
 
   popupMessage = (popupMessage) => this.setState({popupMessage});
 
@@ -101,8 +114,9 @@ class Landscape extends Component {
     return;
   }
 
-  render() { 
-    return (      
+  render() {
+    const {objects, bookStack, tooltip, showTooltip} = this.state;
+    return (
       <div id="landscape-variant-container" 
            className="bottom-container" 
            style={{ 
@@ -111,46 +125,67 @@ class Landscape extends Component {
            }}>
         <div className="rel-container">
           <img src={require('../assets/landscape/landscape-1.png')} className="landscape" id="landscape-1" alt="landscape 1" />
-          {this.state.objects[0] &&
-          this.state.objects.map(obj => {
-            const props = {
-              key: obj.name,
-              id: obj.name,
-              className: 'landscape-object',
-              style: {
-                left: obj.left,
-                top:  obj.top,
-              },
-              onClick: this.handleObjectClick
+          {objects[0] &&
+            objects.map(obj => {
+              const props = {
+                key: obj.id,
+                id: obj.id,
+                name: obj.name,
+                className: 'landscape-object',
+                style: {
+                  left: obj.left,
+                  top:  obj.top,
+                },
+                onClick: this.handleObjectClick,
+                onMouseOver: this.showTooltip,
+                onMouseOut: this.hideTooltip
+              }
+              
+              if (obj.id === 'book-stack') {
+                return bookStack ? (
+                  <div {...props} style={{...props.style, width: obj.width, height: obj.height}}>
+                    {
+                      bookStack.map((b, i) => 
+                        <img 
+                          src={require('../assets/box-dark.png')} 
+                          className="book--tiny" 
+                          key={`book-${i}`} 
+                          alt="book"
+                          style={{
+                            height: BOOK_HEIGHT,
+                            width: BOOK_WIDTH,
+                            bottom: b.yOffset,
+                            left: b.xOffset,
+                            filter: `brightness(${b.tintDeviation})`
+                          }}
+                        />)
+                    }
+                  </div>
+                ) : null;
+              }
+              else {
+                return <img {...props} src={require(`../assets/landscape/objects/${obj.id}.png`)} alt={obj.id} />
+              }
             }
-            
-            if (obj.name === 'book-stack') {
-              return this.state.bookStack ? (
-                <div {...props} style={{...props.style, width: obj.width, height: obj.height}}>
-                  {
-                    this.state.bookStack.map((b, i) => 
-                      <img 
-                        src={require('../assets/box-dark.png')} 
-                        className="book--tiny" 
-                        key={`book-${i}`} 
-                        alt="book"
-                        style={{
-                          height: BOOK_HEIGHT,
-                          width: BOOK_WIDTH,
-                          bottom: b.yOffset,
-                          left: b.xOffset,
-                          filter: `brightness(${b.tintDeviation})`
-                        }}
-                      />)
-                  }
-                </div>
-              ) : null;
-            }
-            else {
-              return <img {...props} src={require(`../assets/landscape/objects/${obj.name}.png`)} alt={obj.name} />
-            }
-          }
           )}
+
+          <CSSTransition
+            in={showTooltip}
+            classNames="tooltip"
+            unmountOnExit
+            timeout={500}
+          >
+            <p
+              className="tooltip"
+              style={{
+                left: tooltip && tooltip.left,
+                top: tooltip && tooltip.top,
+                fontSize: tooltip && tooltip.fontSize
+              }}
+            >
+              {tooltip && tooltip.contents}
+            </p>
+          </CSSTransition>
         </div>
       </div>
     );
