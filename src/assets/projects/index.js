49295bbbc0e3fe63ba4499dd8cf0d98fa35d9738
Jirinrin <jiri.ei@hotmail.com>
2019-01-13 17:30:00 +0100

    Fixed some image styling and added a bunch of architecture images

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -5,49 +5,94 @@ export default [
     id: 'architecture-highlights',
     title: 'Architecture Highlights',
     github: false,
-    images: []
+    images: [
+      'arch-1.png',
+      'arch-2.png',
+      'arch-3.png',
+      'arch-4.png',
+      'arch-5.png',
+      'arch-6.png',
+      'arch-7.png',
+      'arch-8.png',
+      'arch-9.png',
+      'arch-10.png',
+      'arch-11.png',
+      'arch-12.png',
+      'arch-12a.png',
+      'arch-13.png',
+      'arch-14.png',
+      'arch-15.png',
+      'arch-16.png',
+      'arch-17.png',
+      'arch-18.png',
+      'arch-19.png',
+      'arch-20.png',
+      'arch-21.png',
+      'arch-22.png',
+    ]
   },
   {
     id: 'bath-srpn',
     title: 'SRPN',
     github: true,
-    images: []
+    images: [
+      'srpn-1.gif'
+    ]
   },
   {
     id: 'bath-dungeon-of-doom',
     title: 'Dungeon of Doom',
     github: true,
-    images: ['cat1.jpg', 'cat2.jpg', 'cat3.jpg', 'cat4.jpg']
+    images: [
+      'dod-2.gif',
+      'dod-3.gif',
+      'dod-4.gif'
+    ]
   },
   {
     id: 'first-ever-website',
     title: 'First Evarrr Website',
     github: true,
-    images: [] /// alleen de namen, het pad kan het script uiteindelijk zelf wel uitvogelen
+    images: [
+      'first-website.png'
+    ]
   },
   {
     id: 'codaisseur-project-cowbell',
     title: 'Project Cowbell',
     github: true,
-    images: []
+    images: [
+      'cowbell-1.gif'
+    ]
   },
   {
     id: 'semicolon-spammer',
     title: ';;;;;;;;',
     github: true,
-    images: []
+    images: [
+      'semicolon-spammer-1.gif',
+      'semicolon-spammer-2.gif'
+    ]
   },
   {
     id: 'week7-bomberman-game',
     title: 'Bomberman clone',
     github: true,
-    images: []
+    images: [
+      'bomberman-1.gif',
+      'bomberman-2.gif',
+      'bomberman-3.gif'
+    ]
   },
   {
     id: 'week8-ticket-uber-fullstack',
     title: 'Ticket Uber',
     github: true,
-    images: []
+    images: [
+      'ticket-uber-2.gif',
+      'ticket-uber-3.gif',
+      'ticket-uber-4.gif'
+    ]
   },
   {
     id: 'portfolio-website',
@@ -59,6 +104,11 @@ export default [
     id: 'questionnaires-appjection',
     title: 'Questionnaire creator',
     github: false,
-    images: []
+    images: [
+      'appjection-1.gif', 
+      'appjection-2.gif', 
+      'appjection-3.gif', 
+      'appjection-4.gif'
+    ]
   },
 ]
