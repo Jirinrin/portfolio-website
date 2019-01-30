d3991c8645990233c80fa8c8812e73b8e97217e8
Jirinrin <jiri.ei@hotmail.com>
2019-01-30 16:45:21 +0100

    Added Google Analytics and warning in the SOUL message

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Provider} from 'react-redux';
 import { CookiesProvider } from 'react-cookie';
+import ReactGA from 'react-ga';
 
 import store from './store';
 
@@ -11,13 +12,19 @@ import LandscapeContainer from './components/LandscapeContainer';
 import './App.scss';
 
 class App extends Component {
+  componentDidMount() {
+    ReactGA.initialize('UA-133506146-1');
+    ReactGA.set({ anonymizeIp: true });
+    ReactGA.pageview(window.location.pathname);
+  }
+
   render() {
     return (
       <CookiesProvider>
         <Provider store={store}>
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
             <div className="App">
-            {/* /// dat showaboutoptions kan via redux evt */}
+              {/* /// dat showaboutoptions kan via redux evt */}
               <Navbar showAboutOptions={false} />
               <div id="main">
                 <GithubCodeContainer />
--- src/assets/objects/jiri-soul.md
+++ src/assets/objects/jiri-soul.md
@@ -7,3 +7,6 @@ As you could probably tell from how she looks, she is really just a child, who a
 I like to think that my SOUL's neverending wonder at what life can be like and willingness to absorb as much as she can, truly gives me the power to always continue to develop myself and put as much passion into projects as I do~~~
 
 _(© Tomorrow Corporation™, 1809 for the image I traced this character from)_
+
+_Oh, and here is a disclaimer about this site because I don't know where else to put it:_  
+I am using Google Analytics cookies for investigating the demographics that visit this site and e.g. what browsers they use. Even though I have a data processing agreement, no data is shared with Google, Google has no permission to do anything with the data and your IP address is anonymised. So yeey, nothing to worry about! And because of this I don't have to ask for your permission~ (/^▽^)/
